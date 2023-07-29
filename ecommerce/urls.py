fc3ffe034c48317a3365bc1396ce23c6e268df65
abbas <abbas@pop-os.localdomain>
2023-07-29 11:01:07 +0330

    nothing change

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
  	path('', include('products.urls', namespace='mainapp')),
  	# path('', include('checkout.urls', namespace='checkout')),
     path('admin/', admin.site.urls),
-    # path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
